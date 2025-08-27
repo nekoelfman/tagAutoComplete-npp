9b130249aa21828cf3a3de559f5ecdf703d7a8e6
nekoelfman <dev-github@ishikazu.sakuratan.com>
2025-08-27 16:59:42 +0900

    全体的に作り直し。区切り文字の処理を追加。

--- incrementWordStrength.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from wordStrength import process_word_at_cursor
-
-# ----------------------------------------------------------------------------------------
-# タグの重みを増やす
-
-process_word_at_cursor(True)
